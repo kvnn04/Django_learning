fda072f035855096c242bbe240f42a01b239f507
Kevin Herrera <kvnherrera.04@gmail.com>
2025-01-06 12:43:50 -0300

    Primer commit: Hice un pantallazo general sobre flask, estructura, vista, modelos

---
+++ miProyecto/tasks/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Task
+# Create your views here.
+
+def task_list(request):
+    tasks = Task.objects.all()
+    print("Tareas en la base de datos:", tasks)
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
